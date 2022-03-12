e96bada0d399a83db2e6dbb88b910128ac7d4b28
hugosenari <hugosenari@gmail.com>
2022-03-12 02:52:54 -0300

    chore: re index lc

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = ''A post-modern, "multi-dimensional" configurable ls/file lister'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."lc-master".url = "path:./master";
+  inputs."lc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_2".url = "path:./v0_2";
+  inputs."lc-v0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_3".url = "path:./v0_3";
+  inputs."lc-v0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_4".url = "path:./v0_4";
+  inputs."lc-v0_4".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_4".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_5".url = "path:./v0_5";
+  inputs."lc-v0_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_6".url = "path:./v0_6";
+  inputs."lc-v0_6".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_6".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_7".url = "path:./v0_7";
+  inputs."lc-v0_7".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_7".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_8".url = "path:./v0_8";
+  inputs."lc-v0_8".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_8".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_8_1".url = "path:./v0_8_1";
+  inputs."lc-v0_8_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_8_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."lc-v0_9_0".url = "path:./v0_9_0";
+  inputs."lc-v0_9_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lc-v0_9_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ v0_3/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''A post-modern, "multi-dimensional" configurable ls/file lister'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-lc-v0_3.flake = false;
+  inputs.src-lc-v0_3.owner = "c-blake";
+  inputs.src-lc-v0_3.ref   = "refs/tags/v0.3";
+  inputs.src-lc-v0_3.repo  = "lc";
+  inputs.src-lc-v0_3.type  = "github";
+  
+  inputs."cligen".dir   = "nimpkgs/c/cligen";
+  inputs."cligen".owner = "riinr";
+  inputs."cligen".ref   = "flake-pinning";
+  inputs."cligen".repo  = "flake-nimble";
+  inputs."cligen".type  = "github";
+  inputs."cligen".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."cligen".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-lc-v0_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-lc-v0_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
